45e2769ebc2c0cac93290ebf09214813ad77ef7d
YunaPark <jeannelvn@gmail.com>
2023-06-27 18:26:06 +0900

    add: movie app start

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        // 현재의 값을 가져와서 새로운 배열로 반환한다.
-        setToDos((current) => [toDo, ...current]);
-        setToDo("");
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(
+            `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
     };
-    const onClick = (i) => {
-        setToDos(toDos.filter((v, toIdx) => i !== toIdx));
-    };
-
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-            <h1>Todo's ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo}
-                    type="text"
-                    placeholder="입력 해주세요😉"
-                />
-                <button>Add</button>
-            </form>
-            <hr />
-            <ul>
-                {toDos.map((v, i) => (
-                    <li key={i}>
-                        {v}
-                        {/* onClick={deleteBtn} 이 아닌onClick={() => deleteBtn(index)} 이렇게 쓰는 이유는 "바로 실행"되지 않고 클릭을 기다리는 함수로 쓰기 위함 */}
-                        <button onClick={() => onClick(i)}>x</button>
-                    </li>
-                ))}
-            </ul>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img src={movie.medium_cover_image} />
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
