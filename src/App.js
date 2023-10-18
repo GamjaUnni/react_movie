b48ab3704ccd595a2c7f10f2a513cda265506aab
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-18 14:26:28 +0900

    add: todo list 복습

--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./routes/Home";
-// import Detail from "./routes/Detail";
 import { useState } from "react";
 
-function App() {
-    const [toDo, setTodo] = useState("");
-    const [toDos, setToDos] = useState([]);
+const App = () => {
+    const [value, setValue] = useState("");
+    const [todo, setTodo] = useState([]);
     const onChange = (e) => {
-        setTodo(e.target.value);
+        setValue(e.target.value);
     };
-
     const onSubmit = (e) => {
         e.preventDefault();
-        if (toDo === "") return;
-        setToDos((currentArray) => [toDo, ...currentArray]);
-        setTodo("");
+        if (value === "") {
+            return;
+        }
+        setTodo((curr) => [value, ...curr]);
+        setValue("");
     };
-
-    const onClick = (i) => {
-        console.log(i);
-        setToDos(toDos.filter((v, idx) => idx !== i));
+    const onclick = (i) => {
+        setTodo(todo.filter((v, idx) => idx !== i));
     };
     return (
         <div>
-            <h1>My To Dos({toDos.length})</h1>
+            <h1>to do list</h1>
             <form onSubmit={onSubmit}>
                 <input
-                    type="test"
-                    value={toDo}
+                    type="text"
+                    value={value}
                     onChange={onChange}
-                    placeholder="write here!"
+                    placeholder="할일을 입력하세요"
                 />
                 <button>add</button>
             </form>
             <ul>
-                {toDos.map((v, i) => (
+                {todo.map((v, i) => (
                     <li key={i}>
                         {v}
-                        <button onClick={() => onClick(i)}>X</button>
+                        <button onClick={() => onclick(i)}>X</button>
                     </li>
                 ))}
             </ul>
         </div>
-
-        // <Router>
-        //     <Routes>
-        //         <Route path="/movie/:id" element={<Detail />}></Route>
-        //         <Route path="/" element={<Home />}></Route>
-        //     </Routes>
-        // </Router>
     );
-}
+};
 
 export default App;
