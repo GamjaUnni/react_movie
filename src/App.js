a42f32d4b7b92a01b6540c9ced23728b7751437b
[SW5] 박유나 <jeannelvn@gmail.com>
2023-10-19 15:48:18 +0900

    add: Movie 컴포넌트 분리, Route 연결

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-    const [coins, setCoins] = useState([]);
-    useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then((response) => response.json())
-            .then((json) => {
-                setCoins(json);
-                setLoading(false);
-            });
-    }, []);
     return (
-        <div>
-            <h1>The Coins!({coins.length})</h1>
-            {loading ? <strong>Loading...</strong> : null}
-            <ul>
-                {coins.map((v, i) => (
-                    <li key={v.id}>{v.name} (v.symbol)</li>
-                ))}
-            </ul>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/movie">
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
     );
 };
 
---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Movie from "../components/Movie";
+
+const Home = () => {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const movieData = await (
+            await fetch(
+                `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+            )
+        ).json();
+        setMovies(movieData.data.movies);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                "loading..."
+            ) : (
+                <div>
+                    {movies.map((v) => (
+                        <Movie
+                            key={v.id}
+                            coverImg={v.medium_cover_image}
+                            title={v.title}
+                            summary={v.summary}
+                            genres={v.genres}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+export default Home;
